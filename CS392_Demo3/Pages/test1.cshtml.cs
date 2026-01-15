62c92c339f2ea621801ce7ae2cd7b650355845d2
Mini Zeng <beckymini.mz0005@hotmail.com>
2026-01-15 15:50:23 -0500

    add test1 page under page folder

---
+++ CS392_Demo3/Pages/test1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS392_Demo3.Pages
+{
+    public class test1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
